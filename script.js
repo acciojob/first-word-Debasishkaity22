5fc1952af4fcc3bb31d48206fb36ffd21f68ad3d
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-19 16:18:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function firstWord(s) {
   // your code here
+	if(s.length===0||(s.length===1 && s.charAt(0)===" ")) return "";
+	let ans="";
+	for(let i=0;i<s.length;i++){
+    debugger
+		if(s.charAt(i)===" "){
+			 ans=s.slice(i+1);
+		}
+		else{
+			break;
+		}
+	}
+	if(ans.length===0){
+		let res=s.split(" ");
+	return res[0];
+	}
+	else{
+		let res=ans.split(" ");
+		return res[0];
+	}
 }
-
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
